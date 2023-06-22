31c4342d3a5fde13a274ccceb72a6218c780de0c
yacoubou-seidou <yacoseidou@gmail.com>
2023-06-22 15:51:10 +0100

    Make required changes

--- rental.rb
+++ rental.rb
@@ -9,4 +9,14 @@ class Rental
 
   attr_accessor :date
   attr_reader :book, :person
+
+  def book=(book)
+    @book = book
+    book.rentals << self unless book.rentals.include?(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.rentals << self unless person.rentals.include?(self)
+  end
 end
