4109610f17f1fca688aea85cfb8a7c20880aa1e0
yacoubou-seidou <yacoseidou@gmail.com>
2023-06-22 14:47:53 +0100

    Set up associations

--- student.rb
+++ student.rb
@@ -8,6 +8,11 @@ class Student < Person
     @classroom = classroom
   end
 
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
+  end
+
   def play_hooky
     '¯\(ツ)/¯'
   end
