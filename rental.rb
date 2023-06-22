24bd8afda0e7b87c9cc32b5007cc51fb7e0a1045
yacoubou-seidou <yacoseidou@gmail.com>
2023-06-22 15:38:46 +0100

    Make required changes

--- rental.rb
+++ rental.rb
@@ -1,20 +1,12 @@
 class Rental
-  attr_accessor :date
-  attr_reader :book, :person
-
   def initialize(date, book, person)
     @date = date
     @book = book
+    book.rentals << self
     @person = person
+    person.rentals << self
   end
 
-  def book=(book)
-    @book = book
-    book.rentals << self unless book.rentals.include?(self)
-  end
-
-  def person=(person)
-    @person = person
-    person.rentals << self unless person.rentals.include?(self)
-  end
+  attr_accessor :date
+  attr_reader :book, :person
 end
